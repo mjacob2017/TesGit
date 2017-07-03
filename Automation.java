a1d56a8d877fb5160c2df1251bb9e3de51295e1c
mjacob2017 <mjacobchacko@yahoo.com>
2017-07-02 20:42:55 -0400

    Updated java file

--- Automation.java
+++ Automation.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Automation {
+public class Automation  {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","C:\\SeleniumDrivers\\chromedriver_win32\\chromedriver.exe");
